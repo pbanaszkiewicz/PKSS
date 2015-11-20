d94f91293fec0cc72e09651e24450edb0dc0b377
seunghooppasaranghae <nnnnodahlia@gmail.com>
2015-11-20 22:29:38 +0100

    Create howToGetData.py
    Program przykładowy do pobierania danych z logów experymentów w Pythonie

---
+++ howToGetData.py
@@ -0,0 +1,30 @@
+#po odpaleniu tego pliku, pod warunkiem ze w sciezce data/ex6.txt znaduje
+#sie poprawny plik, wyswietli nam sie wektor czasu zarejestrowany
+#podczas eksperymentu.
+
+#potrzebna biblioteka
+import ast
+
+#otwieramy wybrany plik
+file = open('data/ex6.txt','r')
+
+#zapisujemy zawartosc pliku do listy fileContent
+#1 element listy = 1 linia z pliku
+fileContent = file.readlines()
+
+#tworzymy liste na dana, ktora chcemy odczytac z pliku
+myData = []
+
+#dla kazdej linii z pliku...
+for line in fileContent:
+  #do zmiennej temp wpisuje 'doslowna ewaluacje', czyli interpretuje
+  #string z pliku jak linie pythona. W ten sposob w zmiennej 'temp' 
+  #znajduje sie zmienna typu slownik dla danej linii.
+  temp = ast.literal_eval(line)
+  
+  #do listy zawierajacej moja dana wpisuje jej wartosc ze slownika.
+  #w tym przykladzie pobieram wartosci zmiennej 'time'
+  myData.append(temp['time'])
+
+#po zakonczeniu petli w liscie myData znajduje sie wektor danych  
+print myData
